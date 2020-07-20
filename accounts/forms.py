430dc3b76ca2051e777acc14abe05965ebd0d91f
Dragoshiz <dragosimbrea@gmail.com>
2020-07-20 22:29:32 +0300

    user form not working anyad

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import ContactPerson    
+from .models import ContactPerson, User    
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 
@@ -8,3 +8,10 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = ContactPerson
         fields = ['first_name', 'last_name', 'email', 'phone', 'title']
+
+    def clean(self):
+        cleaned_data = super(RegisterForm, self).clean()
+        email_exists = User.objects.filter(email=cleaned_data['email']).exists()
+        if email_exists:
+            raise forms.ValidationError("Email already exists, anyad!")
+        return cleaned_data
--- accounts/views.py
+++ accounts/views.py
@@ -1,17 +1,26 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+
 from .forms import RegisterForm
 
 
 # Create your views here.
-def register(response):
-    if response.method == 'POST':
-        form = RegisterForm(response.POST)
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
         if form.is_valid():
             form.save()
-        return redirect('blog')
+            messages.success(request, 'Congratulations, Anyad')
+            return redirect('blog')
+        else:
+            import ipdb; ipdb.set_trace()
+            for msg in form.error_messages:
+                messages.error(request, f"{msg}: {form.error_messages[msg]}")
+            messages.error(request, "Something went wrong")
+            return redirect('register')
     else:
         form = RegisterForm()
 
-    return render(response, 'accounts/register.html', {'form': form})
+    return render(request, 'accounts/register.html', {'form': form})
